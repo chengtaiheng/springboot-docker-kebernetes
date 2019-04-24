c0cc9454480c70524f03d63c5552de80c065ce0c
chth <chth668@163.com>
2019-04-24 09:59:35 +0800

    Initial commit

---
+++ src/main/java/springboot/docker/k8s/K8sApplication.java
@@ -0,0 +1,16 @@
+package springboot.docker.k8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: 程泰恒
+ * @date: 2019/4/23 14:18
+ */
+
+@SpringBootApplication
+public class K8sApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(K8sApplication.class, args);
+    }
+}
---
+++ src/main/java/springboot/docker/k8s/controller/K8sController.java
@@ -0,0 +1,29 @@
+package springboot.docker.k8s.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author: 程泰恒
+ * @date: 2019/4/23 14:20
+ */
+
+@RestController
+@RequestMapping("/kebernetes")
+public class K8sController {
+
+    @GetMapping("/test")
+    public Map getResult() {
+
+        HashMap<String, Object> resultMap = new HashMap<String, Object>();
+
+        resultMap.put("result", "successful");
+
+        return resultMap;
+    }
+
+}
